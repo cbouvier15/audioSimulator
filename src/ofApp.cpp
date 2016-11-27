b0568eeb838f346e8114e487e8f2ae6aac8736c5
Christian Bouvier <cbouvier@trea.uy>
2016-11-26 23:08:51 -0300

    OSC /blobAudio and /backgroundAudio with GUI access

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,70 +2,57 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    // OSC
+    sender.setup(SEND_TO_IP, SEND_TO_PORT);
+    
+    // GUI
+    gui.setup("audioSimulator");
+    gui.setShape(30, 30, 200, 100);
+    gui.add(sendBlobAudioMessage.setup("Send audioBlob", false));
+    gui.add(sendBackgroundMessage.setup("Send audioBack", false));
+    gui.add(desiredFramerate.setup("FrameRate",20,1,60));
+    
+    // Frame rate
+    ofSetFrameRate(desiredFramerate);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    if (sendBlobAudioMessage){
+        this->sendBlobAudio();
+    }
+    
+    if (sendBackgroundMessage){
+        this->sendBackgroundAudio();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofBackgroundGradient(ofColor::white, ofColor::gray);
+    ofSetHexColor(0xffffff);
+    this->gui.draw();
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::sendBlobAudio(){
+    // sending blobAudio
+    ofxOscMessage m;
+    m.setAddress("/blobAudio");
+    m.addInt32Arg(0);
+    float y = (float) ofGetMouseY() / (float) ofGetHeight();
+    m.addFloatArg(y);
+    sender.sendMessage(m);
+    ofLog(OF_LOG_NOTICE, "Sent /blobAudio " + ofToString(y));
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::sendBackgroundAudio(){
+    // sending backgroundAudio
+    ofxOscMessage m;
+    m.setAddress("/backgroundAudio");
+    float y = (float) ofGetMouseY() / (float) ofGetHeight();
+    m.addFloatArg(y);
+    sender.sendMessage(m);
+    ofLog(OF_LOG_NOTICE, "Sent /backgroundAudio " + ofToString(y));
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,24 +1,28 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOsc.h"
+#include "ofxGui.h"
+
+#define SEND_TO_IP "127.0.0.1"
+#define SEND_TO_PORT 12347
 
 class ofApp : public ofBaseApp{
 
+    ofxOscSender sender;
+    
+    void sendBlobAudio();
+    void sendBackgroundAudio();
+    
+    //Interface
+    ofxPanel gui;
+    ofxToggle sendBlobAudioMessage;
+    ofxToggle sendBackgroundMessage;
+    ofxIntSlider desiredFramerate;
+    ofxLabel text;
+    
 	public:
 		void setup();
 		void update();
 		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
 };
